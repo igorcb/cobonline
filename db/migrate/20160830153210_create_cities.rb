4ba865ef01e032ad330c7d975e6fd9e394ba5f6b
Igor C. Batista <igor.batista@gmail.com>
2016-08-30 12:37:24 -0300

    db uniq name cities

--- db/migrate/20160830153210_create_cities.rb
+++ db/migrate/20160830153210_create_cities.rb
@@ -1,7 +1,7 @@
 class CreateCities < ActiveRecord::Migration
   def change
     create_table :cities do |t|
-      t.string :name
+      t.string :name, limit: 100, null: false
 
       t.timestamps null: false
     end
