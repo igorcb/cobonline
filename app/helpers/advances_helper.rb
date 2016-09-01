32491313df685e786f08c863976740b5af53e941
Igor C. Batista <igor.batista@gmail.com>
2016-09-01 16:39:39 -0300

    navlink

--- app/helpers/advances_helper.rb
+++ app/helpers/advances_helper.rb
@@ -1,2 +1,17 @@
 module AdvancesHelper
+  def full_title(page_title)
+    base_title = "Grupo Sucesso Car"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end	
+
+  def nav_link(text, path)
+	  options = current_page?(path) ? { class: "active" } : {}
+	  content_tag(:li, options) do
+	    link_to text, path
+	  end
+  end	
 end
