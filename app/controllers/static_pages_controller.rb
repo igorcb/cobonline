0552dd100011b2d2b7b69880972c01dbf6cb5631
Igor C. Batista <igor.batista@gmail.com>
2016-09-08 18:11:59 -0300

    swicth Date.today for Date.current 2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,6 @@ class StaticPagesController < ApplicationController
   before_action :user_admin
 
 	def dashboard
-    @item_advances = ItemAdvance.joins(:client, :city).select("cities.name as cidade, sum(item_advances.price) as valor, sum(item_advances.value_payment) as valor_pago").where("DATE(due_date) = ? ", Date.today.to_s).group("cities.name").order('cities.name')
+    @item_advances = ItemAdvance.joins(:client, :city).select("cities.name as cidade, sum(item_advances.price) as valor, sum(item_advances.value_payment) as valor_pago").where("DATE(due_date) = ? ", Date.current.to_s).group("cities.name").order('cities.name')
 	end
 end
