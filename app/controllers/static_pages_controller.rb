c6b0d9e55f9a9f339dd5ba6949308c5d9d65887b
Igor C. Batista <igor.batista@gmail.com>
2016-09-08 07:57:55 -0300

    authenticate and user admin to dashboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :user_admin
+
 	def dashboard
     @item_advances = ItemAdvance.joins(:client, :city).select("cities.name as cidade, sum(item_advances.price) as valor, sum(item_advances.value_payment) as valor_pago").where("DATE(due_date) = ? ", Date.today.to_s).group("cities.name").order('cities.name')
 	end
