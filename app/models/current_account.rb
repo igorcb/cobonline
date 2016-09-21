cde7e958f902e04328644b0958bb777352eb11d6
Igor C. Batista <igor.batista@gmail.com>
2016-09-21 17:15:42 -0300

    summary current account for operator

--- app/models/current_account.rb
+++ app/models/current_account.rb
@@ -7,6 +7,9 @@ class CurrentAccount < ActiveRecord::Base
   validates :date_ocurrence, presence: true
   validates :type_launche, presence: true
   validates :price, presence: true
+
+  scope :total_creditos, -> { where(type_launche: TypeLaunche::CREDITO) }
+  scope :total_debitos, -> { where(type_launche: TypeLaunche::DEBITO) }
   
   module TypeLaunche
     DEBITO = -1
@@ -25,7 +28,6 @@ class CurrentAccount < ActiveRecord::Base
     ['date_ocurrence', 'city_id', 'cost_id', 'historic' ]
   end
 
-
   # def self.saldo(date=nil)
   #   date.nil? ? CurrentAccount.sum('price*type_launche') : CurrentAccount.where(date_ocurrence: date).sum('price*type_launche')
   # end
@@ -34,4 +36,15 @@ class CurrentAccount < ActiveRecord::Base
     date.nil? ? CurrentAccount.where(city_id: city).sum('price*type_launche') : CurrentAccount.where(city_id: city, date_ocurrence: date).sum('price*type_launche')
   end
 
+  def self.saldo_anterior(city)
+    CurrentAccount.where("current_accounts.city_id = ? and DATE(current_accounts.date_ocurrence) < ? ", city, Date.today.to_s).sum('price*type_launche')
+  end
+
+  def self.total_credito(city)
+    CurrentAccount.total_creditos.where("current_accounts.city_id = ? and DATE(current_accounts.date_ocurrence) = ? ", city, Date.today.to_s).sum('price')
+  end
+
+  def self.total_debito(city)
+    CurrentAccount.where("current_accounts.city_id = ? and DATE(current_accounts.date_ocurrence) = ? ", city, Date.today.to_s).sum('price')
+  end
 end
